0c167e1f48f0d76ec0ae5f41a1b67336a7566f61
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-24 15:31:42 +0100

    Add the unit test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe "StaticPages", type: :request do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
     end
+
+    it "has the correct title" do
+      get "/static_pages/home"
+      assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    end
   end
 
   describe "GET /help" do
@@ -13,12 +18,22 @@ RSpec.describe "StaticPages", type: :request do
       get "/static_pages/help"
       expect(response).to have_http_status(:success)
     end
+
+    it "has the correct title" do
+      get "/static_pages/help"
+      assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    end
   end
 
   describe "GET /about" do
     it "returns http success" do
       get "/static_pages/about"
       expect(response).to have_http_status(:success)
+    end
+
+    it "has the correct title" do
+      get "/static_pages/about"
+      assert_select "title", "About | Ruby on Rails Tutorial Sample App"
     end
   end
 end
